c9e292b056635ee94fc998f9aa4cebc6888b6f2d
bandpassdev <noahtovares@gmail.com>
2014-01-15 21:57:30 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,20 +71,21 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "return value of authenticate method" do
-    before { @user.save }
+describe "return value of authenticate method" do
+  before { @user.save }
+  let(:found_user) { User.find_by(email: @user.email) }
 
-    let (:found_user) {User.find_by(email: @user.email)}
-
-    describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password)}
-    end
-
-    describe "with invalid pasword" do
-      let (:user_for_invalid_password) {found_user.authenticate("invalid")}
-      specify {expect(user_for_invalid_password).to be_false }
-    end
+  describe "with valid password" do
+    it { should eq found_user.authenticate(@user.password) }
   end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not eq user_for_invalid_password }
+    specify { expect(user_for_invalid_password).to be_false }
+  end
+
 
   describe "with a password that's too short" do
   before { @user.password = @user.password_confirmation = "a" * 5 }
