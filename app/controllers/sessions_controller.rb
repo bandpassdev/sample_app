fb824c4ffd7a2dc34fd85927f74bc1017f442640
bandpassdev <noahtovares@gmail.com>
2014-01-16 17:53:49 -0500

    finished re-doing chapter 8, starting chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 			user = User.find_by(email: params[:session][:email].downcase)
 			if user && user.authenticate(params[:session][:password])
 				sign_in user
-				redirect_back_or user
+				redirect_to user
 			else
-				flash.now[:error] = 'Invalid email/password combination' # NOT QUITE RIGHT
+				flash.now[:error] = 'Invalid email/password combination' 
 				render 'new'
 			end
 	end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,6 +63,10 @@ describe "User pages" do
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
       
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
 
   end
