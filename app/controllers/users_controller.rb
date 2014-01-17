5e5807c1a9d43ee77d804b17ee48f554e1a6dc3b
bandpassdev <noahtovares@gmail.com>
2014-01-16 23:19:45 -0500

    friendly forwarding working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+ def edit
+  end
+  
   def index
     @users = User.all
   end
@@ -25,12 +28,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+ 
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -41,16 +41,19 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-     :password_confirmation)
-  end
+ def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
     # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
+
 
     def correct_user
       @user = User.find(params[:id])
